bd142daaa84aabdd39ca1afd285fe94416209397
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-03 00:36:44 +0300

    g

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -41,7 +41,7 @@ public class F1 extends Car {
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
-            setCurrentGear(1);
+            //setCurrentGear(1);
         }
         //for all other cases, change the gear accordingly
 
